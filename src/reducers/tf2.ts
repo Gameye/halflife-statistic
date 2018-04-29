1c9e9dbdd42120ef4facb95a8a4a4091a6394072
matthijs <matthijs@kadijk.com>
2018-04-29 16:27:05 +0200

    start new reducer

--- src/reducers/tf2.ts
+++ src/reducers/tf2.ts
@@ -6,6 +6,7 @@ import { Tf2Patch, Tf2State } from "../state";
 export class Tf2LogReducer extends LogReducerBase<Tf2State, Tf2LogEvents>
 {
     // TODO: implement the reducer ;-)
+    private gameOver = false;
 
     protected createParser() {
         return new Tf2LogParser();
@@ -26,9 +27,87 @@ export class Tf2LogReducer extends LogReducerBase<Tf2State, Tf2LogEvents>
         event: Tf2LogEvents,
     ): Iterable<Tf2Patch> {
         // yield* this.reduceSettingEvent(event);
-        // yield* this.reduceStartStopEvent(event);
+        yield* this.reduceStartStopEvent(event);
         // yield* this.reduceRoundStartStopEvent(event);
         // yield* this.reducePlayerEvent(event);
         // yield* this.reduceTeamEvent(event);
+    }
+
+    protected *reduceStartStopEvent(
+        event: Tf2LogEvents,
+    ): Iterable<Tf2Patch> {
+        const state = this.getState();
+
+        switch (event.type) {
+            case "round-start": {
+                const start = event.payload.timestamp;
+                if (!state.start) yield {
+                    path: ["start"],
+                    value: start,
+                };
+                break;
+            }
+
+            case "game-commencing": {
+                const start = event.payload.timestamp;
+                yield {
+                    path: ["start"],
+                    value: start,
+                };
+                break;
+            }
+
+            case "game-over": {
+                const stop = event.payload.timestamp;
+                yield {
+                    path: ["stop"],
+                    value: stop,
+                };
+                break;
+            }
+        }
+    }
+
+    protected *reduceRoundStartStopEvent(
+        event: Tf2LogEvents,
+    ): Iterable<Tf2Patch> {
+        const { gameOver } = this;
+        const state = this.getState();
+
+        switch (event.type) {
+            case "match-start": {
+                this.gameOver = false;
+                yield {
+                    path: ["startedRounds"],
+                    value: 0,
+                };
+                yield {
+                    path: ["finishedRounds"],
+                    value: 0,
+                };
+                break;
+            }
+            case "round-start": {
+                if (gameOver) break;
+                yield {
+                    path: ["startedRounds"],
+                    value: state.startedRounds + 1,
+                };
+                break;
+            }
+            case "round-end": {
+                if (gameOver) break;
+                yield {
+                    path: ["finishedRounds"],
+                    value: state.finishedRounds + 1,
+                };
+                break;
+            }
+            case "game-over": {
+                if (gameOver) break;
+                this.gameOver = true;
+                break;
+            }
+        }
     }
 }
